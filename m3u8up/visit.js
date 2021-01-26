7992f84f029c39426b389364cadad2273a61a81d
mixizy <76725774+mixizy@users.noreply.github.com>
2021-01-26 13:54:04 +0800

    Create visit.js

---
+++ m3u8up/visit.js
@@ -0,0 +1,12 @@
+{
+  "downurl": "http://d0.ananas.chaoxing.com/",
+  "ref":"http://pan-yz.chaoxing.com/?s=",
+  "openurl":"https://pan.baidu.com/",
+  "name":"M3U8生成器 V1.5.0.exe",
+  "load":"0",
+  "version": "1.4",
+  "exit":"stop",
+  "log": "更新内容：\r\n1.暂无说明\r\n2.暂无说明\r\n",
+  "err":"青山不变绿水长流\r\n感谢大家的一路支持\r\n服务器维护中……",
+  "token":"102"
+}
