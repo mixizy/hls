5415a3eb374e89a00750f82af9a2e6235f5ec1e2
mixizy <76725774+mixizy@users.noreply.github.com>
2021-01-27 18:39:41 +0800

    Update visit.js

--- m3u8up/visit.js
+++ m3u8up/visit.js
@@ -1,14 +1,14 @@
 {
-  "downurl": "http://d0.ananas.chaoxing.com/",
+  "downurl": "https://i7.meixincdn.com/v1/img/T1khdPB7VT1RCvBVdK.png",
   "ref":"http://pan-yz.chaoxing.com/?s=",
-  "openurl":"https://pan.baidu.com/",
+  "openurl":"https://i7.meixincdn.com/v1/img/T1khdPB7VT1RCvBVdK.png",
   "name":"M3U8生成器 V1.4.3.exe",
   "load":"0",
   "version": "1.4",
-  "smallver":"2",
+  "smallver":"3",
   "exit":"stop",
-  "md5":"",
-  "log": "更新内容：\r\n1.暂无说明\r\n2.暂无说明\r\n",
+  "md5":"ee989f88d62d700513c419b01a669413",
+  "log": "更新内容：\r\n1.修复点击开始操作后快速按空格导致重复执行导致出错\r\n2.提示：如果自动下载失败，请下载弹出的地址文件，然后修改后缀为exe即可\r\n",
   "err":"青山不变绿水长流\r\n感谢大家的一路支持\r\n服务器维护中……",
   "token":"102"
 }
