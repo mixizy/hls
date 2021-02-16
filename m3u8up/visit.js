421c11a13c3af8bf4919d41716f6a4e2a9729949
mixizy <76725774+mixizy@users.noreply.github.com>
2021-02-16 12:15:46 +0800

    Update visit.js

--- m3u8up/visit.js
+++ m3u8up/visit.js
@@ -1,14 +1,14 @@
 {
-  "downurl": "https://i7.meixincdn.com/v1/img/T1khdPB7VT1RCvBVdK.png",
+  "downurl": "https://i7.meixincdn.com/v1/img/T1NtZLBmd_1RCvBVdK.png",
   "ref":"http://pan-yz.chaoxing.com/?s=",
-  "openurl":"https://i7.meixincdn.com/v1/img/T1khdPB7VT1RCvBVdK.png",
-  "name":"M3U8生成器 V1.4.3.exe",
+  "openurl":"https://i7.meixincdn.com/v1/img/T1NtZLBmd_1RCvBVdK.png",
+  "name":"M3U8生成器 V1.4.4.exe",
   "load":"0",
   "version": "1.4",
-  "smallver":"3",
+  "smallver":"4",
   "exit":"stop",
-  "md5":"ee989f88d62d700513c419b01a669413",
-  "log": "更新内容：\r\n1.修复点击开始操作后快速按空格导致重复执行导致出错\r\n2.提示：如果自动下载失败，请下载弹出的地址文件，然后修改后缀为exe即可\r\n",
+  "md5":"3d746c4482af32cf69b178c5dacf7ec1",
+  "log": "更新内容：\r\n1.调整伪装助力，把头条挪到最后，疑似玩坏\r\n2.提示：如果自动下载失败，请下载弹出的地址文件，然后修改后缀为exe即可\r\n",
   "err":"青山不变绿水长流\r\n感谢大家的一路支持\r\n服务器维护中……",
   "token":"102"
 }
