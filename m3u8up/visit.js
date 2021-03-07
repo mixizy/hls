0e7e81c76681bc6a3127299549db585f11a889c8
mixizy <76725774+mixizy@users.noreply.github.com>
2021-03-07 19:35:11 +0800

    Update visit.js

--- m3u8up/visit.js
+++ m3u8up/visit.js
@@ -1,14 +1,17 @@
 {
-  "downurl": "https://i7.meixincdn.com/v1/img/T1NtZLBmd_1RCvBVdK.png",
+  "downurl": "https://i7.meixincdn.com/v1/img/T17KdLB7Dv1RCvBVdK.png",
   "ref":"http://pan-yz.chaoxing.com/?s=",
-  "openurl":"https://i7.meixincdn.com/v1/img/T1NtZLBmd_1RCvBVdK.png",
-  "name":"M3U8生成器 V1.4.4.exe",
+  "openurl":"https://i7.meixincdn.com/v1/img/T17KdLB7Dv1RCvBVdK.png",
+  "stopurl":"1",
+  "starurl":"1",
+  "name":"M3U8生成器 V1.4.5.exe",
   "load":"0",
   "version": "1.4",
-  "smallver":"4",
+  "smallver":"5",
+  "release":"[14.5]",
   "exit":"stop",
-  "md5":"3d746c4482af32cf69b178c5dacf7ec1",
-  "log": "更新内容：\r\n1.调整伪装助力，把头条挪到最后，疑似玩坏\r\n2.提示：如果自动下载失败，请下载弹出的地址文件，然后修改后缀为exe即可\r\n",
+  "md5":"2d4a28bb5ac4dcb896a796dde590c1a9",
+  "log": "更新内容：\r\n1.修复部分m3u8获取视频时长出错；\r\n2.添加版本兼容(从1.4.5开始)，如上个版本代码兼容，则老版本依旧正常使用；\r\n3.添加支持自动加入视频模式，由于程序只支持m3u8地址下载而已，开发其他大文件下载又得写一堆代码，所以直接弄了个检测加入，达到利用其他软件下载文件后自动加入上传，方便又好用；\r\n4.提示：如果自动下载失败，请下载弹出的地址文件，然后修改后缀为exe即可\r\n",
   "err":"青山不变绿水长流\r\n感谢大家的一路支持\r\n服务器维护中……",
   "token":"102"
 }
