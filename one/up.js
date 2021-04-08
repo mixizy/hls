d3d9ae95ccc12e72cae2a33fb8d0c4113510a3a5
mixizy <76725774+mixizy@users.noreply.github.com>
2021-04-08 17:07:10 +0800

    Create up.js

---
+++ one/up.js
@@ -0,0 +1,18 @@
+{
+  "downurl": "http://d0.ananas.chaoxing.com/download/84c4ddf00bc3f3e8553519a14940ebae",
+  "ref":"http://pan-yz.chaoxing.com/?s=",
+  "openurl":"https://wwa.lanzous.com/iDyrRf63hzg",
+  "stopurl":"1",
+  "starturl":"1",
+  "name":"电视剧一键获取入库 v3.7.0.exe",
+  "ua":"Yunziliao.",
+  "load":"-2",
+  "version": "3.8",
+  "smallver":"3",
+  "release":"[3.8.3]",
+  "exit":"stop",
+  "md5":"0e3132914535e9234cc573202425d0ee",
+  "log": "更新内容：\r\n1.新增没图片时，自动按片名生成图片\r\n2.新增网站批量预告入库，网站再多，入库无忧\r\n3.优化其他细节\r\n",
+  "err":"兔兔那么可爱\r\n你为什么要吃兔兔\r\n你猜……",
+  "token":"3838438"
+}
